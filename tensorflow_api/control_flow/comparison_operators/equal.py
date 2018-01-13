133031a75695a42359df674e18f3f66e8b370885
hwauni <hwauni1@gmail.com>
2018-01-13 16:16:34 +0900

    studied comparison operators

---
+++ tensorflow_api/control_flow/comparison_operators/equal.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import tensorflow as tf
+import tfutil
+
+
+if __name__ == '__main__':
+    for xs in [(0, 0), (1, 0)]:
+        const1, const2 = tf.constant(xs[0]), tf.constant(xs[1])
+
+        # y : A  Tensor . Must have the same type as  x 
+        y = tfutil.get_operation_value(tf.equal(const1, const2))
+        print(str(xs) + " -> " + str(y))
---
+++ tensorflow_api/control_flow/comparison_operators/less_equal.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import tensorflow as tf
+import tfutil
+
+
+if __name__ == '__main__':
+    for xs in [(0, 0), (0, 1), (1, 0)]:
+        const1, const2 = tf.constant(xs[0]), tf.constant(xs[1])
+
+        # y : A  Tensor . Must have the same type as  x 
+        y = tfutil.get_operation_value(tf.less_equal(const1, const2))
+        print(str(xs[0]) + " <= " + str(xs[1]) + " -> " + str(y))
